b0629d06321c19239145cd21fbcb88856209a9de
1082cubi <1082cubi@hanmail.net>
2023-11-09 11:12:37 +0900

    package.json에 proxyhttp://localhost:8080 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { BoardList } from "./page/BoardList";
+import { BoardWrite } from "./page/BoardWrite";
+import { HomeLayout } from "./layout/HomeLayout";
 
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<HomeLayout />}>
+      <Route path="write" element={<BoardWrite />} />
+      <Route index element={<BoardList />} />
+    </Route>,
+  ),
+);
 function App(props) {
-  return (
-    <div></div>
-  );
+  return <RouterProvider router={routes}></RouterProvider>;
 }
 
 export default App;
