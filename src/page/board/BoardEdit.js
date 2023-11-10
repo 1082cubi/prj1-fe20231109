5c207f3db9cd349a122cfd873e2f8e39b712890a
1082cubi <1082cubi@hanmail.net>
2023-11-10 12:36:04 +0900

    암호 확인 로직 추가

--- src/page/board/BoardEdit.js
+++ src/page/board/BoardEdit.js
@@ -4,8 +4,17 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
   Textarea,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 import { useImmer } from "use-immer";
 import React, { useEffect } from "react";
@@ -17,6 +26,8 @@ export function BoardEdit() {
   // /edit/:id
   const { id } = useParams();
   const navigate = useNavigate();
+  const toast = useToast();
+  const { isOpen, onClose, onOpen } = useDisclosure();
 
   useEffect(() => {
     axios
@@ -35,11 +46,28 @@ export function BoardEdit() {
     //PUT
     axios
       .put("/api/board/edit", board)
-      .them(() => console.log("잘됨"))
-      .catch(() => console.log("잘안됨"))
-      .final(() => console.log("끝"));
-    console.log(board);
+      .then(() => {
+        toast({
+          description: board.id + "번 게시글이 수정되었습니다",
+          status: "success",
+        });
+        navigate("/board/" + id);
+      })
+      .catch((error) => {
+        if (error.response.status === 400) {
+          toast({
+            description: "요청이 잘못되었습니다",
+            status: "error",
+          });
+        } else {
+          toast({
+            description: "수정 중에 문제가 발생하였습니다",
+          });
+        }
+      })
+      .finally(() => onClose());
   }
+
   return (
     <Box>
       <h1>{id}번 글 수정</h1>
@@ -76,10 +104,28 @@ export function BoardEdit() {
           }
         />
       </FormControl>
-      <Button colorScheme="blue" onClick={handleSubmit}>
+      <Button colorScheme="blue" onClick={onOpen}>
         저장
       </Button>
+      {/* navigate(-1) : 이전 경로로 이동 */}
       <Button onClick={() => navigate(-1)}>취소</Button>
+
+      {/* 수정 모달 */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>저장 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>저장 하시겠습니까?</ModalBody>
+
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button onClick={handleSubmit} colorScheme="blue">
+              저장
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
